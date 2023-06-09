2232329b6d23a3253ec1f15846f4fcbbc124dd42
fra <98514696+francescopapaleo@users.noreply.github.com>
2023-06-09 17:02:37 +0200

    adding comments and small fixes

--- wekirouter.py
+++ wekirouter.py
@@ -1,6 +1,12 @@
+'''
+By default, Wekinator listens for its input messages on port 6448. The default input message is 
+/wek/inputs and each input must be sent as a float within this message. This router is intended
+for use with it'''
+
 from pythonosc.dispatcher import Dispatcher
 from pythonosc import osc_server
 from pythonosc import udp_client
+
 
 new_message = {
     "/gesture/mouth/width": 0.0,
@@ -22,9 +28,10 @@ def raw_handler(unused_addr, *args):
     pass
 
 if __name__ == '__main__':
-    ip = "127.0.0.1"
-    receiving_from = 8338
-    sending_to = 6448
+
+    ip = "127.0.0.1"            # Wekinator's IP address
+    receiving_from = 8338       # Port to receive messages from
+    sending_to = 6448           # Port to send messages to
 
     client = udp_client.SimpleUDPClient(ip, sending_to)
     print("Sending to {} on port {}".format(ip, sending_to))
@@ -32,10 +39,20 @@ if __name__ == '__main__':
     dispatcher = Dispatcher()
     dispatcher.map('/gesture/mouth/width', package_gesture)
     dispatcher.map('/gesture/mouth/height', package_gesture)
+    
     # Map other gestures here like the two above
+
 
     dispatcher.map("/raw", raw_handler)
 
-    server = osc_server.ThreadingOSCUDPServer((ip, receiving_from), dispatcher)
-    print("Serving on {}".format(server.server_address))
-    server.serve_forever()
+    try:
+        server = osc_server.ThreadingOSCUDPServer((ip, receiving_from), dispatcher)
+        print("Serving on {}".format(server.server_address))
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        print("\nServer has been stopped by the user.")
+        
+        # Optionally add any cleanup here
+
+
