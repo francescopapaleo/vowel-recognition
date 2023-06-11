4ec79ab457619e8195dca629259fc19008c67d93
fra <98514696+francescopapaleo@users.noreply.github.com>
2023-06-11 17:28:33 +0200

    extract formants

--- audio_osc_wekinator.py
+++ audio_osc_wekinator.py
@@ -5,38 +5,47 @@ import numpy as np
 
 print(librosa.__version__)
 
+sample_rate = 44100
+
 # Initialize OSC client
 client = udp_client.SimpleUDPClient('127.0.0.1', 6448)
 
-sample_rate = 44100
-n_fft = 512  # FFT window size
-hop_length = 256  # number of samples between successive frames
-n_mels = 128  # number of Mel bands
+
+def extract_formants(audio, sample_rate):
+    formants = librosa.lpc(audio, order=10)                     # LPC to get LPF coefs
+    roots = np.roots(formants) 
+    roots = roots[np.imag(roots) >= 0]                          # Only take roots in the upper half-plane
+    frequencies = np.arctan2(np.imag(roots), np.real(roots)) \
+    * (sample_rate / (2 * np.pi))                               # Convert LPC > frequency
+    frequencies = np.sort(frequencies)                          # Sort frequencies in ascending order
+    return frequencies[0], frequencies[1], frequencies[2]       # Return the first 3, which are enough to identify the vowel
+
 
 def audio_callback(indata, frames, time, status):
     volume_norm = np.linalg.norm(indata) * 10
-    print(f"Volume norm: {volume_norm}")
+    if status:
+        print(f"Status: {status}")
+    
+    if any(indata):
+        # Convert the incoming audio to a NumPy array
+        buffer = np.squeeze(indata)
+    else:
+        print("No input data")
 
-    audio_data = indata[:, 0]
-    print(f"Audio data shape: {audio_data.shape}")
-    print(f"Audio data type: {type(audio_data)}")
-    print(f"Audio data first 10 elements: {audio_data[:10]}")
-
-# ------------------- Audio Feature Extraction ------------------- #
-
+    # Process the current audio frame using librosa
+    f1, f2, f3 = extract_formants(buffer, sample_rate)
+    
+    formants = [float(f1), float(f2), float(f3)] # This line is changed to cast the formants to regular Python float type
+    client.send_message("/wek/inputs", formants)
 
 
-
-
-
-
-
-# ---------------------------------------------------------------- #
-
-    client.send_message("/wek/inputs", audio_data.flatten().tolist())
+# Query available input devices
+input_devices = sd.query_devices()
+input_device_name = input_devices[0]["name"]  # Access the first device in the tuple and retrieve the name
 
 # Start the audio stream and capture microphone input
-with sd.InputStream(callback=audio_callback, channels=1, samplerate=sample_rate):
+with sd.InputStream(callback=audio_callback, channels=1, samplerate=sample_rate, device=0):
+    print(f"Selected audio device: {input_device_name}")
     print("Audio stream started. Press Ctrl+C to stop.")
     try:
         while True:
@@ -45,3 +54,4 @@ with sd.InputStream(callback=audio_callback, channels=1, samplerate=sample_rate)
         print("Audio stream stopped.")
     except Exception as e:
         print(f"Error in audio_callback: {e}")
+
